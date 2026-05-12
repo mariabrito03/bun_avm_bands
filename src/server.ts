8c98617ec3fcbb88431e93fb775598200e7ca306
MBRICAL <merybri03@gmail.com>
2026-05-12 15:59:48 +0200

    mbrical: Creación del store y del service

--- src/server.ts
+++ src/server.ts
@@ -8,14 +8,18 @@ export const creareServer = () => {
 
     const engine = new Engine({ path: SERVER_CONFIG.path });
 
-    io.bind(engine);
+io.bind(engine);
 
-    io.on("connection", (socket) => {
+io.on("connection", (socket) => {
     console.log(`Cliente conectado (socket.id): ${socket.id}`);
 
     socket.emit("saludo", "Hola desde el servidor");
 
     socket.on("chat", (msg)=> io.emit("chat", msg));
+})
+
+io.on("disconnect", (socket) => {
+    console.log(`Cliente desconectado: ${socket.id}`);
 })
 
     const { fetch: engineFetch, websocket } = engine.handler();
